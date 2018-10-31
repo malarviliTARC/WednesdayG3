37af17e73a38123354dd2f425623ae4df371cfe6
Tarc <Tarc@PC161>
2018-10-31 16:04:06 +0800

    completed story add product - updated error

--- src/g3sample/G3sample.java
+++ src/g3sample/G3sample.java
@@ -16,6 +16,7 @@ public class G3sample {
      */
     public static void main(String[] args) {
         System.out.println("Hello world 1");
+         System.out.println("Hello world 2");
     }
     
 }
